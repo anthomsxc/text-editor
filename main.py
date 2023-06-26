fc75d7e79ac1eaa698c5d033f5827cc8f2c61921
anthomsxc <131298953+anthomsxc@users.noreply.github.com>
2023-06-26 17:55:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,11 @@ def createFile():
    
     os.system("cls")
 
-    file = input("Nom du fichier à créer : ")
+    file = input("Nom du fichier : ")
 
-    lignes = ['']
+    with open(file, 'r+') as file_to_write:
+        lignes = file_to_write.readlines()
+        lignes_backup = lignes.copy()
 
     current_line = 0
 
@@ -39,36 +41,37 @@ def createFile():
         for numero, ligne in enumerate(lignes, 1):
             num = Back.LIGHTBLUE_EX + str(numero) + Style.RESET_ALL
             if numero - 1 == current_line:
-                
-                print(f"> {num} {ligne}")
+                print(f"> {num} {ligne.strip()}")
             else:
-                print(f"  {num} {ligne}")
+                print(f"  {num} {ligne.strip()}")
 
         key = ord(msvcrt.getch())
 
         if key == 224: 
             key = ord(msvcrt.getch())
 
-            if key == 80:
+            if key == 80: 
                 current_line = min(current_line + 1, len(lignes) - 1)
-            elif key == 72:  # Up arrow
+            elif key == 72:
                 current_line = max(current_line - 1, 0)
 
-        elif key == 13:
+        elif key == 13:  # touche entrer
             new_text = input("Nouveau texte : ")
-            lignes[current_line] = new_text
+            lignes[current_line] = new_text + '\n'
 
-        elif key == 19:
+        elif key == 26:  # touche retour
+            if current_line < len(lignes):
+                lignes[current_line] = lignes_backup[current_line]
+
+        elif key == 19:  # touche sauvegarde
             with open(file, 'w') as file_to_write:
-                file_to_write.write('\n'.join(lignes))
+                file_to_write.writelines(lignes)
             break
 
-        elif key == 27: 
-            print("Fichier en cours de sauvegarde !")
+        elif key == 27: # touche quitter
+            print("en cours de sauvegarde...")
             time.sleep(0.5)
             print("aurevoir...")
-            time.sleep(1)
-            mainInterface()
             break
 
 
