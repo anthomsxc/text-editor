ff13859237d5f3af8e97e22c2b403f4d20dfaa72
anthomsxc <131298953+anthomsxc@users.noreply.github.com>
2023-06-26 17:45:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,29 +39,33 @@ def createFile():
         for numero, ligne in enumerate(lignes, 1):
             num = Back.LIGHTBLUE_EX + str(numero) + Style.RESET_ALL
             if numero - 1 == current_line:
-                # Highlight the current line
+                
                 print(f"> {num} {ligne}")
             else:
                 print(f"  {num} {ligne}")
 
         key = ord(msvcrt.getch())
 
-        if key == 224:  # Special keys (arrow keys, function keys, etc.)
-            key = ord(msvcrt.getch())  # Get the actual key code
+        if key == 224: 
+            key = ord(msvcrt.getch())
 
-            if key == 80:  # Down arrow
+            if key == 80:
                 current_line = min(current_line + 1, len(lignes) - 1)
             elif key == 72:  # Up arrow
                 current_line = max(current_line - 1, 0)
 
-        elif key == 13:  # Enter key
+        elif key == 13:
             new_text = input("Nouveau texte : ")
             lignes[current_line] = new_text
 
-        elif key == 19:  # Ctrl+S
+        elif key == 19:
             with open(file, 'w') as file_to_write:
                 file_to_write.write('\n'.join(lignes))
-            print("Fichier créé et sauvegardé !")
+            print("Fichier en cours de sauvegarde !")
+            time.sleep(0.5)
+            print("aurevoir...")
+            time.sleep(1)
+            mainInterface()
             break
 
         elif key == 27:  # Escape key
